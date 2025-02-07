7568a5cdfbce1c86b8ad633015e86dd98be9d333
Sebin Suresh <sebinsuresh@gmail.com>
2025-02-06 19:35:36 -0600

    refactor: reorganize and put p5 functions on top

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -59,102 +59,6 @@ const sketch = (p) => {
     document.addEventListener("touchcancel", touchHandler, { passive: false });
   };
 
-  function setupPalette() {
-    swatchWidth = p.min(swatchWidth, Math.floor((p.width + 8) / NumColors));
-    for (let i = 0; i < NumColors; i++) {
-      PaletteColors.push(Math.floor(i * (255 / (NumColors - 1))));
-    }
-    currentFillColor = PaletteColors[p.floor(NumColors / 2)];
-  }
-
-  function drawPalette() {
-    let currIndex = 0;
-    for (let i = 0; i < NumColors; i++) {
-      if (currentFillColor === PaletteColors[i]) {
-        currIndex = i;
-      }
-      p.noStroke();
-      p.fill(PaletteColors[i]);
-      p.square(StartPaletteX + i * swatchWidth, StartPaletteY, swatchWidth);
-    }
-    // show outline for active swatch
-    p.strokeWeight(4);
-    p.stroke(255, 200);
-    p.noFill();
-    p.square(StartPaletteX + currIndex * swatchWidth, StartPaletteY, swatchWidth);
-
-    // cursor
-    if (currentState !== IS_DRAWING && isInputOverPalette(p.mouseX, p.mouseY)) {
-      p.cursor(p.HAND);
-    } else {
-      p.cursor(p.ARROW);
-    }
-  }
-
-  function drawHelpText() {
-    p.stroke(0);
-    p.fill(255);
-    p.textSize(16);
-    p.text(
-      `Press 1 - ${NumColors} for colors.
-Press 'Esc' to cancel stroke, Press 'R' to reset canvas.
-'Ctrl + Z' to undo - (${undoStates.length}) remaining.
-enable iOS Safari 120hz: Settings > Apps > Safari > Advanced > Feature flags > Turn off "prefer page rendering updates near 60fps"`,
-      StartPaletteX,
-      StartPaletteY + swatchWidth + 20
-    );
-  }
-
-  /**
-   * @param {number} x
-   * @param {number} y
-   */
-  function isInputOverPalette(x, y) {
-    return (
-      x > StartPaletteX &&
-      y > StartPaletteY &&
-      x < StartPaletteX + swatchWidth * NumColors &&
-      y < StartPaletteY + swatchWidth
-    );
-  }
-
-  function drawPainting() {
-    p.image(paintingGfx, 0, 0);
-  }
-
-  function drawActive() {
-    p.image(activeStrokeGfx, 0, 0);
-  }
-
-  function clearCanvas() {
-    p.background(0);
-  }
-
-  /**
-   * @param {number[][]} points
-   * @returns {number[]} The [minX, minY, maxX, maxY] values
-   */
-  function getBoundingBox(points) {
-    const boundingBox = [p.width, p.height, 0, 0];
-    for (let i = 0; i < points.length; i++) {
-      let currentPoint = points[i];
-      if (currentPoint[0] < boundingBox[0]) {
-        boundingBox[0] = currentPoint[0];
-      }
-      if (currentPoint[0] > boundingBox[2]) {
-        boundingBox[2] = currentPoint[0];
-      }
-
-      if (currentPoint[1] < boundingBox[1]) {
-        boundingBox[1] = currentPoint[1];
-      }
-      if (currentPoint[1] > boundingBox[3]) {
-        boundingBox[3] = currentPoint[1];
-      }
-    }
-    return boundingBox;
-  }
-
   p.windowResized = () => {
     // resizeCanvas(windowWidth, windowHeight);
     // paintingGfx.resizeCanvas(windowWidth, windowHeight);
@@ -263,37 +167,6 @@ enable iOS Safari 120hz: Settings > Apps > Safari > Advanced > Feature flags > T
     currentState = IS_HOVERING;
   };
 
-  function pushUndoState() {
-    if (currentStrokeVertices.length > 1) {
-      const bb = getBoundingBox(currentStrokeVertices);
-
-      // Fix issue when minX=maxX or minY=maxY, causing drawImage error for width/height=0
-      let w = bb[2] - bb[0];
-      if (w === 0) {
-        bb[0] -= 1;
-        bb[2] += 1;
-        w = bb[2] - bb[0];
-      }
-      let h = bb[3] - bb[1];
-      if (h === 0) {
-        bb[1] -= 1;
-        bb[3] += 1;
-        h = bb[3] - bb[1];
-      }
-
-      const undoSnippet = paintingGfx.get(bb[0], bb[1], w, h);
-      const numUndoStates = undoStates.push({ img: undoSnippet, boundingBox: bb });
-      if (numUndoStates > MaxUndos) {
-        undoStates.splice(0, 1);
-      }
-    }
-  }
-
-  function persistActiveAndClear() {
-    paintingGfx.image(activeStrokeGfx, 0, 0);
-    activeStrokeGfx.clear();
-  }
-
   p.keyPressed = () => {
     if (currentState === IS_DRAWING && p.keyCode === p.ESCAPE) {
       currentState = IS_HOVERING;
@@ -324,6 +197,133 @@ enable iOS Safari 120hz: Settings > Apps > Safari > Advanced > Feature flags > T
       1;
     }
   };
+
+  function setupPalette() {
+    swatchWidth = p.min(swatchWidth, Math.floor((p.width + 8) / NumColors));
+    for (let i = 0; i < NumColors; i++) {
+      PaletteColors.push(Math.floor(i * (255 / (NumColors - 1))));
+    }
+    currentFillColor = PaletteColors[p.floor(NumColors / 2)];
+  }
+
+  function drawPalette() {
+    let currIndex = 0;
+    for (let i = 0; i < NumColors; i++) {
+      if (currentFillColor === PaletteColors[i]) {
+        currIndex = i;
+      }
+      p.noStroke();
+      p.fill(PaletteColors[i]);
+      p.square(StartPaletteX + i * swatchWidth, StartPaletteY, swatchWidth);
+    }
+    // show outline for active swatch
+    p.strokeWeight(4);
+    p.stroke(255, 200);
+    p.noFill();
+    p.square(StartPaletteX + currIndex * swatchWidth, StartPaletteY, swatchWidth);
+
+    // cursor
+    if (currentState !== IS_DRAWING && isInputOverPalette(p.mouseX, p.mouseY)) {
+      p.cursor(p.HAND);
+    } else {
+      p.cursor(p.ARROW);
+    }
+  }
+
+  function drawHelpText() {
+    p.stroke(0);
+    p.fill(255);
+    p.textSize(16);
+    p.text(
+      `Press 1 - ${NumColors} for colors.
+Press 'Esc' to cancel stroke, Press 'R' to reset canvas.
+'Ctrl + Z' to undo - (${undoStates.length}) remaining.
+enable iOS Safari 120hz: Settings > Apps > Safari > Advanced > Feature flags > Turn off "prefer page rendering updates near 60fps"`,
+      StartPaletteX,
+      StartPaletteY + swatchWidth + 20
+    );
+  }
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
+  function isInputOverPalette(x, y) {
+    return (
+      x > StartPaletteX &&
+      y > StartPaletteY &&
+      x < StartPaletteX + swatchWidth * NumColors &&
+      y < StartPaletteY + swatchWidth
+    );
+  }
+
+  function drawPainting() {
+    p.image(paintingGfx, 0, 0);
+  }
+
+  function drawActive() {
+    p.image(activeStrokeGfx, 0, 0);
+  }
+
+  function clearCanvas() {
+    p.background(0);
+  }
+
+  /**
+   * @param {number[][]} points
+   * @returns {number[]} The [minX, minY, maxX, maxY] values
+   */
+  function getBoundingBox(points) {
+    const boundingBox = [p.width, p.height, 0, 0];
+    for (let i = 0; i < points.length; i++) {
+      let currentPoint = points[i];
+      if (currentPoint[0] < boundingBox[0]) {
+        boundingBox[0] = currentPoint[0];
+      }
+      if (currentPoint[0] > boundingBox[2]) {
+        boundingBox[2] = currentPoint[0];
+      }
+
+      if (currentPoint[1] < boundingBox[1]) {
+        boundingBox[1] = currentPoint[1];
+      }
+      if (currentPoint[1] > boundingBox[3]) {
+        boundingBox[3] = currentPoint[1];
+      }
+    }
+    return boundingBox;
+  }
+
+  function pushUndoState() {
+    if (currentStrokeVertices.length > 1) {
+      const bb = getBoundingBox(currentStrokeVertices);
+
+      // Fix issue when minX=maxX or minY=maxY, causing drawImage error for width/height=0
+      let w = bb[2] - bb[0];
+      if (w === 0) {
+        bb[0] -= 1;
+        bb[2] += 1;
+        w = bb[2] - bb[0];
+      }
+      let h = bb[3] - bb[1];
+      if (h === 0) {
+        bb[1] -= 1;
+        bb[3] += 1;
+        h = bb[3] - bb[1];
+      }
+
+      const undoSnippet = paintingGfx.get(bb[0], bb[1], w, h);
+      const numUndoStates = undoStates.push({ img: undoSnippet, boundingBox: bb });
+      if (numUndoStates > MaxUndos) {
+        undoStates.splice(0, 1);
+      }
+    }
+  }
+
+  function persistActiveAndClear() {
+    paintingGfx.image(activeStrokeGfx, 0, 0);
+    activeStrokeGfx.clear();
+  }
 };
 
 new p5(sketch);
